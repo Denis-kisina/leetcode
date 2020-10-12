4c0558a04b58915c064cb4b8834585e6bab9dd46
Denis Kisina <kisinad@gmail.com>
2020-10-12 03:51:28 -0500

    removed the spring core folder

--- springcore/src/main/java/denis/kisina/EmailService.java
+++
@@ -1,8 +0,0 @@
-package denis.kisina;
-
-public class EmailService {
-
-    public void sendEmail(String receiver, String message){
-        System.out.println(receiver + " is get the following " + message);
-    }
-}
